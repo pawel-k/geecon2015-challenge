5f0003fe776ec9e3afce71df751c774ba612a2d7
Pawel Kaczmarczyk <pawel.kaczmarczyk@mail.com>
2015-05-15 00:03:52 +0200

    implementation

--- src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
+++ src/main/java/pl/allegro/promo/geecon2015/domain/ReportGenerator.java
@@ -2,17 +2,23 @@ package pl.allegro.promo.geecon2015.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
 import pl.allegro.promo.geecon2015.domain.stats.FinancialStatisticsRepository;
+import pl.allegro.promo.geecon2015.domain.stats.FinancialStats;
 import pl.allegro.promo.geecon2015.domain.transaction.TransactionRepository;
+import pl.allegro.promo.geecon2015.domain.transaction.UserTransaction;
 import pl.allegro.promo.geecon2015.domain.user.UserRepository;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Component
 public class ReportGenerator {
-    
+
     private final FinancialStatisticsRepository financialStatisticsRepository;
-    
+
     private final UserRepository userRepository;
-    
+
     private final TransactionRepository transactionRepository;
 
     @Autowired
@@ -26,7 +32,31 @@ public class ReportGenerator {
 
     public Report generate(ReportRequest request) {
         Report report = new Report();
+        FinancialStats financialStats = financialStatisticsRepository.listUsersWithMinimalIncome(request.getMinimalIncome(), request.getUsersToCheck());
+        financialStats.forEach(userId -> report.add(new ReportedUser(userId,
+                getName(userId),
+                getTransactionsAmount(userId))));
         return report;
     }
-    
+
+    private BigDecimal getTransactionsAmount(UUID userId) {
+        try {
+            return transactionRepository.transactionsOf(userId).getTransactions()
+                    .stream()
+                    .map(UserTransaction::getAmount)
+                    .reduce(BigDecimal::add)
+                    .get();
+        } catch (HttpServerErrorException e) {
+            return null;
+        }
+    }
+
+    private String getName(UUID userId) {
+        try {
+            return userRepository.detailsOf(userId).getName();
+        } catch (HttpServerErrorException e) {
+            return "<failed>";
+        }
+    }
+
 }
